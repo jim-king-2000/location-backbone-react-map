4b9ba6e97e74fd0f5fe7f7054924fd4eaaf10a4a
Jim <jim_king_2000@163.com>
2019-10-12 16:13:33 +0800

    add vector marker for mapbox

--- src/hal/international/mapbox/marker.js
+++ src/hal/international/mapbox/marker.js
@@ -1,10 +1,27 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import ReactDOM from 'react-dom';
 
 export class Marker {
   constructor(map, position, options) {
-    this.marker = new mapboxgl.Marker()
+    this.iconContainer = document.createElement('div');
+    ReactDOM.render((
+      <svg
+        preserveAspectRatio='xMidYMid meet'
+        fontSize={options.size || 30}
+        fill={options.fill || 'currentColor'}
+        viewBox='0 0 47.032 47.032'
+        width='30px'
+        height='30px'
+      >
+        <g>
+          <path d={options.svgIcon} />
+        </g>
+      </svg>
+    ), this.iconContainer);
+    this.marker = new mapboxgl.Marker(this.iconContainer)
       .setLngLat([position.longitude, position.latitude])
       .addTo(map);
+      
   }
 
   setPosition(position) {
