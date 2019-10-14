71ef4300f30727553518e0bbb61d459d6a15ee53
Jim <jim_king_2000@163.com>
2019-10-14 10:08:05 +0800

    add polyline for google map

--- src/hal/international/googlemap/map.js
+++ src/hal/international/googlemap/map.js
@@ -1,4 +1,5 @@
 import { Marker } from './marker';
+import { Polyline } from './polyline';
 
 export class GoogleMap {
   constructor(dom) {
@@ -18,5 +19,9 @@ export class GoogleMap {
 
   addMarker(position, options) {
     return new Marker(this.map, position, options);
+  }
+
+  addPolyline(path, options) {
+    return new Polyline(this.map, path, options);
   }
 }
