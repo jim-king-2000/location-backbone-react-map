a79fb6be28d96a5b76f472dbfa940b8c25142adb
Jim <jim_king_2000@163.com>
2019-10-25 13:58:44 +0800

    refactory

--- src/hal/domestic/bmap/mapView.js
+++ src/hal/domestic/bmap/mapView.js
@@ -4,32 +4,13 @@ export class MapView {
   constructor(map) {
     this.zoomOut = () => map.zoomOut();
     this.zoomIn = () => map.zoomIn();
-    this.setFitView = () => map.setViewport(
-      map.getOverlays().map(
-        o => {
-          // console.log(o, o._point, o.getPosition&&o.getPosition(), o.getBounds)
-          // It is a workaround of a bug of Baidu Map API. It creates an overlay
-          // whose name is 'TANGRAM__2c' when InfoWindow is created. And it keeps
-          // alive even when InfoWindow is closed. We have to exclude this overlay
-          // when we run setFitView.
-          if (o.ea === 'TANGRAM__2c')
-            return [];
-  
-          // For CustomOverlay in react-bmap.
-          if (o._point)
-            return o._point;
-  
-          if (o.getPosition)
-            return o.getPosition() || [];
-          
-          if (o.getBounds)
-            return [o.getBounds().getSouthWest(), o.getBounds().getNorthEast()];
-          
-          return [];
-        }).flat());
-    this.isInView = things => {
+    this.setFitView = positions => map.setViewport(
+      positions.filter(p => p.latitude && p.longitude)
+      .map(p => PositionToPoint(p))
+      );
+    this.isInView = positions => {
       const bounds = map.getBounds();
-      return things.filter(p => p.latitude && p.longitude)
+      return positions.filter(p => p.latitude && p.longitude)
         .every(p => bounds.containsPoint(PositionToPoint(p)));
     };
   }
