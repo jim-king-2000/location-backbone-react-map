bff97e6ae247858ed12cb4b9ffdb8492fc870039
Jim <jim_king_2000@163.com>
2019-10-26 11:43:55 +0800

    fix zoom for bing maps

--- src/hal/international/bingmap/mapView.js
+++ src/hal/international/bingmap/mapView.js
@@ -2,8 +2,8 @@ import { PositionToLocation } from './utils';
 
 export class MapView {
   constructor(map) {
-    this.zoomOut = () => map.zoomOut();
-    this.zoomIn = () => map.zoomIn();
+    this.zoomOut = () => map.setView({ zoom: map.getZoom() - 1 });
+    this.zoomIn = () => map.setView({ zoom: map.getZoom() + 1 });
     this.setFitView = positions => {
       const bounds = Microsoft.Maps.LocationRect.fromLocations(
         positions.map(p => PositionToLocation(p))
