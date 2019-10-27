c992cdd22ffda591914548d7e1e178bedec44683
Jim <jim_king_2000@163.com>
2019-10-27 22:28:34 +0800

    modify infobox for bing maps

--- src/hal/international/bingmap/infoWindow.js
+++ src/hal/international/bingmap/infoWindow.js
@@ -4,14 +4,39 @@ import { renderToStaticMarkup } from 'react-dom/server';
 export class InfoWindow {
   constructor(map, options) {
     const { position, events, children, ...others } = options;
+    const htmlContent = renderToStaticMarkup(
+      <div
+        style={{
+          backgroundColor: 'white',
+          borderRadius: '5px',
+          padding: '9px',
+          position: 'relative',
+        }}
+      >
+        <button
+          onClick={events.close}
+          style={{
+            top: 0,
+            right: 0,
+            border: 'none',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            width: '20px',
+            height: '20px',
+            padding: 0,
+            margin: 0,
+            position: 'absolute',
+          }}
+        >
+          &#10006;
+        </button>
+        {children}
+      </div>
+    );
     this.infoWindow = new Microsoft.Maps.Infobox(PositionToLocation(position), {
       ...others,
-      htmlContent: renderToStaticMarkup(children),
+      htmlContent,
     });
     this.infoWindow.setMap(map);
-    // events && Object.entries(events).forEach(
-    //   ([key, value]) => this.infoWindow.on(key, value)
-    // );
   }
 
   setOptions(options) {
