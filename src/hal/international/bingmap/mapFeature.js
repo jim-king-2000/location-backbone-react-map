93b4678e396b07b4139a274f8a89817b7b1ae421
Jim <jim_king_2000@163.com>
2019-10-29 00:10:28 +0800

    refactory

--- src/hal/international/bingmap/mapFeature.js
+++ src/hal/international/bingmap/mapFeature.js
@@ -8,13 +8,13 @@ const customMapStyle = {
 export class MapFeature {
   constructor(map) {
     this.setFullMap = isFullMap => {
-      map.setOptions({
-        customMapStyle: isFullMap ? {} : customMapStyle,
-      });
       map.setView({
         labelOverlay: isFullMap ?
           Microsoft.Maps.LabelOverlay.visible :
           Microsoft.Maps.LabelOverlay.hidden,
+      });
+      map.setOptions({
+        customMapStyle: isFullMap ? {} : customMapStyle,
       });
     };
   }
