0c297c2af0a82253558d755f61a9cc0123a82a6d
Jim <jim_king_2000@163.com>
2019-10-28 09:51:35 +0800

    refactory

--- src/hal/international/bingmap/infoWindow.js
+++ src/hal/international/bingmap/infoWindow.js
@@ -1,40 +1,42 @@
 import { PositionToLocation } from './utils';
 import { renderToStaticMarkup } from 'react-dom/server';
 
+function renderContent(children) {
+  return renderToStaticMarkup(
+    <div
+      style={{
+        backgroundColor: 'white',
+        borderRadius: '5px',
+        padding: '9px',
+        position: 'relative',
+      }}
+    >
+      <button
+        // onClick={events.close}
+        style={{
+          top: 0,
+          right: 0,
+          border: 'none',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          width: '20px',
+          height: '20px',
+          padding: 0,
+          margin: 0,
+          position: 'absolute',
+        }}
+      >
+        &#10006;
+      </button>
+      {children}
+    </div>
+  );
+}
 export class InfoWindow {
   constructor(map, options) {
     const { position, events, children, ...others } = options;
-    const htmlContent = renderToStaticMarkup(
-      <div
-        style={{
-          backgroundColor: 'white',
-          borderRadius: '5px',
-          padding: '9px',
-          position: 'relative',
-        }}
-      >
-        <button
-          onClick={events.close}
-          style={{
-            top: 0,
-            right: 0,
-            border: 'none',
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            width: '20px',
-            height: '20px',
-            padding: 0,
-            margin: 0,
-            position: 'absolute',
-          }}
-        >
-          &#10006;
-        </button>
-        {children}
-      </div>
-    );
     this.infoWindow = new Microsoft.Maps.Infobox(PositionToLocation(position), {
       ...others,
-      htmlContent,
+      htmlContent: renderContent(children),
     });
     this.infoWindow.setMap(map);
   }
@@ -43,7 +45,7 @@ export class InfoWindow {
     const { position, children } = options;
     this.infoWindow.setOptions({
       location: PositionToLocation(position),
-      htmlContent: renderToStaticMarkup(children),
+      htmlContent: renderContent(children),
     });
   }
 
