21524e7fa795aa7b9db809cec1eca45180c63ee0
Jim <jim_king_2000@163.com>
2019-10-28 11:46:03 +0800

    refactory

--- src/utils/DomOverlay.js
+++ src/utils/DomOverlay.js
@@ -3,32 +3,37 @@ import { renderToDiv } from './Render';
 export default fromPositionToPixel => {
   return (
     class {
+      #map;
+      #div;
+      #options;
+      #position;
+
       constructor(position, options) {
-        this.position_ = position;
-        this.options_ = options;
+        this.#position = position;
+        this.#options = options;
       }
     
       onAdd(map) {
-        const div = renderToDiv(this.options_.children);
+        const div = renderToDiv(this.#options.children);
         div.style.visibility = 'hidden';
         div.style.position = 'absolute';
-        div.style.transform = `rotate(${this.options_.angle}deg)`;
+        div.style.transform = `rotate(${this.#options.angle}deg)`;
         div.style.cursor = 'pointer';
-        this.div_ = div;
-        this.map_ = map;
+        this.#div = div;
+        this.#map = map;
         return div;
       }
     
       draw() {
-        const position = fromPositionToPixel(this.position_, this.map_);
-        this.div_.style.left = position.x - this.div_.offsetWidth / 2 + 'px';
-        this.div_.style.top = position.y - this.div_.offsetHeight / 2 + 'px';
-        this.div_.style.visibility = 'visible';
+        const position = fromPositionToPixel(this.#position, this.#map);
+        this.#div.style.left = position.x - this.#div.offsetWidth / 2 + 'px';
+        this.#div.style.top = position.y - this.#div.offsetHeight / 2 + 'px';
+        this.#div.style.visibility = 'visible';
       }
     
       onRemove() {
-        this.div_.parentNode.removeChild(this.div_);
-        this.div_ = null;
+        this.#div.parentNode.removeChild(this.#div);
+        this.#div = null;
       }
     }
   );
