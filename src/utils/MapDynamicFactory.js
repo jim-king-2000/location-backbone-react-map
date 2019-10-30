555f3c3ae81db5bc159f303d31838d59a8e86bd9
Jim <jim_king_2000@163.com>
2019-10-30 12:50:36 +0800

    refactory of dynamic import

--- src/utils/MapDynamicFactory.js
+++ src/utils/MapDynamicFactory.js
@@ -1,17 +1,24 @@
 
-const MapClasses = {
-  RAMap: import('../hal/domestic/amap'),
-  RBMap: import('../hal/domestic/bmap'),
-  RQQMap: import('../hal/domestic/qqmap'),
-  RBingMap: import('../hal/international/bingmap'),
-  RGoogleMap: import('../hal/international/googlemap'),
-  RSogouMap: import('../hal/domestic/sogoumap'),
-  RTianMap: import('../hal/domestic/tianditu'),
-  RHereMap: import('../hal/international/heremap'),
-  RMapBoxGL: import('../hal/international/mapbox-gl'),
-};
-
 export default mapVendor => {
-  const mapClass = MapClasses[mapVendor];
-  return mapClass || RAMap;
+  switch(mapVendor) {
+    case 'RBMap':
+      return import('../hal/domestic/bmap');
+    case 'RQQMap':
+      return import('../hal/domestic/qqmap');
+    case 'RBingMap':
+        return import('../hal/international/bingmap');
+    case 'RGoogleMap':
+        return import('../hal/international/googlemap');
+    case 'RMapBoxGL':
+      return import('../hal/international/mapbox-gl');
+    case 'RHereMap':
+      return import('../hal/international/heremap');
+    case 'RSogouMap':
+      return import('../hal/domestic/sogoumap');
+    case 'RTianMap':
+      return import('../hal/domestic/tianditu');
+    case 'RAMap':
+    default:
+      return import('../hal/domestic/amap');
+  }
 }
