27d292238238076c29871404f2055394288856b4
Jim <jim_king_2000@163.com>
2019-10-30 15:46:47 +0800

    refactory

--- src/utils/CSSLoader.js
+++ src/utils/CSSLoader.js
@@ -1,7 +1,6 @@
 
 export default NativeMapClass => {
   if (!NativeMapClass.buildCssTag) return;
-  if (cache.has(cssFile)) return;
   const cssLink = document.createElement('link');
   cssLink.rel = 'stylesheet';
   cssLink.href = NativeMapClass.buildCssTag();
