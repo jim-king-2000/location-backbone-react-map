b392d2c55f63ad9ddbcc29c4e9352e8561bd28b4
Jim <jim_king_2000@163.com>
2019-10-30 16:29:55 +0800

    refactory

--- src/hal/domestic/bmap/mapView.js
+++ src/hal/domestic/bmap/mapView.js
@@ -6,7 +6,7 @@ export class MapView {
     this.zoomIn = () => map.zoomIn();
     this.setFitView = positions => map.setViewport(
       positions.map(p => PositionToPoint(p))
-      );
+    );
     this.isInView = positions => {
       const bounds = map.getBounds();
       return positions.every(p => bounds.containsPoint(PositionToPoint(p)));
