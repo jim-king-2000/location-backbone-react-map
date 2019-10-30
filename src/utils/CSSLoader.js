31c6ee47b108f8984588c3b56aa95632cf371835
Jim <jim_king_2000@163.com>
2019-10-30 14:58:28 +0800

    add cache

--- src/utils/CSSLoader.js
+++ src/utils/CSSLoader.js
@@ -1,8 +1,12 @@
+const cache = new Set();
 
 export default NativeMapClass => {
   if (!NativeMapClass.buildCssTag) return;
+  const cssFile = NativeMapClass.buildCssTag();
+  if (cache.has(cssFile)) return;
   const cssLink = document.createElement('link');
   cssLink.rel = 'stylesheet';
-  cssLink.href = NativeMapClass.buildCssTag();
+  cssLink.href = cssFile;
   document.head.appendChild(cssLink);
+  cache.add(cssFile);
 }
