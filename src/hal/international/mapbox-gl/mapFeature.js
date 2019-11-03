e351b5591b92793df74ba66102c68cfc69173535
Jim <jim_king_2000@163.com>
2019-11-03 20:13:57 +0800

    refactory

--- src/hal/international/mapbox-gl/mapFeature.js
+++ src/hal/international/mapbox-gl/mapFeature.js
@@ -1,19 +1,3 @@
-const customMapStyle = [{
-  featureType: 'poi',
-  stylers: [{ visibility: 'off' }]
-}, {
-  featureType: 'road',
-  stylers: [{ visibility: 'off' }]
-}, {
-  featureType: 'transit',
-  stylers: [{ visibility: 'off' }]
-}, {
-  featureType: 'administrative',
-  stylers: [{ visibility: 'off' }]
-}, {
-  featureType: 'landscape',
-  stylers: [{ visibility: 'off' }]
-}];
 
 export class MapFeature {
   constructor(map) {
