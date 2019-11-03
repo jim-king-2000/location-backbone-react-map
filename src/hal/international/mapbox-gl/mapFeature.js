d86259965d93df932d93b0e865ca34b42a66d70f
Jim <jim_king_2000@163.com>
2019-11-03 13:00:45 +0800

    refactory

--- src/hal/international/mapbox-gl/mapFeature.js
+++ src/hal/international/mapbox-gl/mapFeature.js
@@ -18,9 +18,8 @@ const customMapStyle = [{
 export class MapFeature {
   constructor(map) {
     this.setFullMap = isFullMap => {
-      map.setOptions({
-        styles: isFullMap ? {} : customMapStyle,
-      });
+      const visibility = isFullMap ? 'visible' : 'none';
+      map.setLayoutProperty('road', 'visibility', visibility);
     };
   }
 }
