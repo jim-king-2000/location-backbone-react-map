d5309428ad8d93f28870619f882159ca9417ef16
Jim <jim_king_2000@163.com>
2019-11-04 09:17:58 +0800

    debug

--- src/hal/international/mapbox-gl/mapFeature.js
+++ src/hal/international/mapbox-gl/mapFeature.js
@@ -2,8 +2,8 @@
 export class MapFeature {
   constructor(map) {
     this.setFullMap = isFullMap => {
-      const opacity = isFullMap ? 1 : 0;
-      map.setLayoutProperty('road', 'Opacity', opacity);
+      const opacity = isFullMap ? 'visible' : 'none';
+      map.setLayoutProperty('road', 'visibility', opacity);
     };
   }
 }
