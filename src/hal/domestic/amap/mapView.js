4f818eed2627774a08a6b6060d919f772868ef6e
Jim <jim_king_2000@163.com>
2019-11-05 15:40:55 +0800

    refactory

--- src/hal/domestic/amap/mapView.js
+++ src/hal/domestic/amap/mapView.js
@@ -3,8 +3,17 @@ import { PositionToLngLat } from './utils';
 export class MapView {
   constructor(map) {
     this.zoomOut = () => map.zoomOut();
+
     this.zoomIn = () => map.zoomIn();
-    this.setFitView = () => map.setFitView();
+
+    this.setFitView = positions => {
+      if (Array.isArray(positions)) return;
+
+      // if (positions.length === 1) map.panTo(PositionToLngLat(positions[0]));
+
+      map.setFitView();
+    };
+
     this.isInView = things => {
       const bounds = map.getBounds();
       return things.every(p => bounds.contains(PositionToLngLat(p)));
