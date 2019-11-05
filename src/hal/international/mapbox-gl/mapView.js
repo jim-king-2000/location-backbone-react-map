998a809f26c5f3336f177d9f17ecda726e1f4c7a
Jim <jim_king_2000@163.com>
2019-11-05 11:37:26 +0800

    fix MapView.isInView for Mapbox

--- src/hal/international/mapbox-gl/mapView.js
+++ src/hal/international/mapbox-gl/mapView.js
@@ -1,6 +1,20 @@
 import { LngLatBounds } from 'mapbox-gl/dist/mapbox-gl.js';
 import { PositionToLngLat } from './utils';
 
+const inBounds = function (bounds, lnglat) {
+  let lng;
+
+  const multLng = (lnglat[0] - bounds['_ne']['lng']) * (lnglat[0] - bounds['_sw']['lng']);
+  if (bounds['_ne']['lng'] > bounds['_sw']['lng']) {
+      lng = multLng < 0;
+  } else {
+      lng = multLng > 0;
+  }
+
+  const lat = (lnglat[1] - bounds['_ne']['lat']) * (lnglat[1] - bounds['_sw']['lat']) < 0;
+  return lng && lat;
+};
+
 export class MapView {
   constructor(map) {
     this.zoomOut = () => map.zoomOut();
@@ -14,7 +28,7 @@ export class MapView {
     };
     this.isInView = things => {
       const bounds = map.getBounds();
-      return things.every(p => bounds.contains(PositionToLngLat(p)));
+      return things.every(p => inBounds(bounds, PositionToLngLat(p)));
     };
   }
 }
