5393a7975a7896dd1db81fb636542354e0bb888e
Jim <jim_king_2000@163.com>
2020-02-03 17:03:04 +0800

    revert the upgrade of amap version

--- src/hal/domestic/amap/map.js
+++ src/hal/domestic/amap/map.js
@@ -33,7 +33,7 @@ export default class RAMap {
 
   static buildScriptTag(mapKey) {
     return [
-      `//webapi.amap.com/maps?v=2.0Beta&key=${mapKey}`,
+      `//webapi.amap.com/maps?v=1.4.15&key=${mapKey}`,
       '//webapi.amap.com/ui/1.0/main.js?v=1.0.11'
     ];
   }
